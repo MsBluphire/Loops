7ec6f93eb6354b19fb3f92796302b5be7c3bc5e6
Denise Williams <williamsde@outlook.com>
2016-08-14 12:14:56 -0400

    first commit

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,22 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+                
+
+            }
+            Console.ReadKey();
         }
     }
 }
